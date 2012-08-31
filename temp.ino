42ae01ae0a7ab376341e53c68c71b9bbd9c2d521
Joakim Jacobsson <jocke@nigobo.net>
2012-08-31 08:16:03 +0200

    Tagit bort allt innehåll i temp.ino

--- temp.ino
+++ temp.ino
@@ -1,25 +1,3 @@
-/*
-  Alexander
-
-  Alexanders own led and button playground.
-
+/**
+ * Den här filen används inte mer
  */
-
-int temp = 10;
-int acctualTemp = 0;
-
-
-// the setup routine runs once when you press reset:
-void setup() {
-
-  Serial.begin(9600) ;
-  pinMode(temp, INPUT);
-
-}
-
-// the loop routine runs over and over again forever:
-void loop() {
-
-  acctualTemp = analogRead(temp);
-  Serial.println(acctualTemp);
-}
